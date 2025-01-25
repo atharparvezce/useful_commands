036b3e683908ad9b6ee70fc5b496e94e520ed5f9
Athar Parvez <47222247+atharparvezce@users.noreply.github.com>
2025-01-25 13:32:37 +0300

    Updation

--- Useful commands.s
+++ Useful commands.s
@@ -1,5 +1,5 @@
 
-# Git Basics Commands
+# Git Commands
 
 # 1. Initialize a new Git repository
 git init
@@ -72,4 +72,73 @@ git branch -d <branch-name>
 
 # 17. Show remote information
 git show origin
+
+
+
+
+# Conda Commands
+
+# 1. Create a new environment
+conda create --name <env-name> <package-names>
+
+
+# 2. Activate an environment
+conda activate <env-name>
+
+
+# 3. Deactivate the current environment
+conda deactivate
+
+
+# 4. List all environments
+conda env list
+
+# 5. Install a package in an environment
+conda install <package-name>
+
+
+# 6. Update a package
+conda update <package-name>
+
+
+# 7. Remove an environment
+conda remove --name <env-name> --all
+# Removes the specified environment and all its packages.
+
+
+# 8. List all installed packages in an environment
+conda list
+# Lists all the installed packages in the current environment.
+
+
+# 9. Export environment to a YAML file
+conda env export > environment.yml
+# Exports the current environment’s dependencies to a `environment.yml` file.
+
+
+# VS Code Commands
+
+# 1. Open VS Code from terminal
+code .
+
+
+# 2. Open a specific file in VS Code
+code <file-path>
+
+
+# 3. List installed extensions
+code --list-extensions
+ 
+
+# 5. Install an extension
+code --install-extension <extension-id>
+
+
+# 6. Uninstall an extension
+code --uninstall-extension <extension-id>
+
+
+# 7. Show VS Code version
+code --version
+
 
